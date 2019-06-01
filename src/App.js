d96dd0746ca6b07aaa89f059fa71d3175646aebc
Hermann <harris.hermann@gmail.com>
2019-06-01 13:06:01 +0200

    graph

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Fragment } from 'react';
 import { Router } from '@reach/router';
 import { ThemeProvider } from 'styled-components';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
 import AppContext from 'components/contextuals/AppContext';
 import AppContextProvider from 'components/contextuals/AppContextProvider';
 import PageNotFound from 'screens/errors/PageNotFound';
@@ -11,6 +16,23 @@ import Protected from './Protected';
 
 import "styles/base.scss";
 
+const httpLink = new HttpLink({
+  uri: "http://localhost:5000/graphql",
+  headers: {
+    'Content-Type' : 'application/json',
+    // authorization: `Bearer ${
+    //   process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+    // }`,
+  },
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
 function App() {
   return (
     <AppContextProvider>
@@ -18,11 +40,13 @@ function App() {
         <AppContext.Consumer>
           {context => (
             <ThemeProvider theme={theme}>
+              <ApolloProvider client={client}>
               <Fragment>
                 {!context.data.isAuthenticated && <Public />}
                 {context.data.isAuthenticated && <Private />}
                 <Protected />
               </Fragment>
+              </ApolloProvider>
             </ThemeProvider>
           )}
         </AppContext.Consumer>
--- src/screens/private/Timeline.js
+++ src/screens/private/Timeline.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
 
 import MatterList from "screens/private/Timeline/MatterList";
 import standard from "screens/private/layouts/standard";
 
+
+const LIST_MATTERS = gql`
+  {
+    matters {
+      title
+      token
+    }
+  }
+`;
+
+
 function Timeline(props) {
   return (
-    <div className="container">
-      <div className="my-5">
-        <MatterList />
-      </div>
-    </div>
+    <Query query={LIST_MATTERS}>
+      {({ data, loading }) => {
+        console.log('graph data', data, loading)
+        return (
+          <div className="container">
+            <div className="my-5">
+              <MatterList />
+            </div>
+          </div>
+        )
+      }}
+    </Query>
   );
 }
 
