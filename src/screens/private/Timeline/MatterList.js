81e97df693b084953ee177849e6f2296c7b616ff
Hermann <harris.hermann@gmail.com>
2019-06-06 21:18:05 +0200

    Modal

--- src/screens/private/Timeline/MatterList.js
+++ src/screens/private/Timeline/MatterList.js
@@ -1,19 +1,19 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Button } from 'react-bootstrap';
 
-
-import NoResults from 'screens/private/components/NoResults'
+import NoResults from 'screens/private/components/NoResults';
 
 import Item from 'screens/private/Timeline/MatterList/Item';
+import MatterModal from 'screens/private/Timeline/MatterModal';
 
 function MatterList({ matters, mattersIndex, removeMatter }) {
-  // const [state, setState] = useState({ matters: {}, mattersIndex: [] });
+  const [showModal, setShowModal] = useState(false);
 
   
   // const [state, setState] = useState({ matters: {a:{title:"Hello World"}, b:{title:"Hello World"}}, mattersIndex: ["a","b"] });
 
   let content = <NoResults />;
-console.log('matters zz', matters)
   if (mattersIndex.length) {
     content = mattersIndex.map( matter => <Item key={matter} token={matter} title={matters[matter].title} removeMatter={removeMatter}/>)
   }
@@ -21,6 +21,8 @@ console.log('matters zz', matters)
   return (
     <div className="container">
       <div className="my-5">
+        <Button variant="primary" onClick={() => setShowModal(!showModal)}>Add Matter</Button>
+        <MatterModal show={showModal} handleClose={() => setShowModal(false)}/>
         {content}
       </div>
     </div>
