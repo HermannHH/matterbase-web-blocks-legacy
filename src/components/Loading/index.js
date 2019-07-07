d7762d0f247fe45965d14ffcf26fd3d901e91dde
Hermann <harris.hermann@gmail.com>
2019-07-07 18:22:08 +0200

    Updates

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -3,9 +3,9 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
 
-export default function Loading({ text }) {
+export default function Loading({ text, fullScreen }) {
   return (
-    <div className="loading">
+    <div className={fullScreen ? "loading full" : "loading"}>
       <FontAwesomeIcon icon={faCircleNotch} size="lg" spin />
       <h5>{ text || "Loading..."}</h5>
     </div>
--- src/contextuals/AppContextProvider.js
+++ src/contextuals/AppContextProvider.js
@@ -1,10 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { getCookie } from 'utils/cookiesHelper';
 import AppContext from './AppContext';
 
-export default function AppContextProvider({ children }) {
-  const [state, setState] = useState({ isAuthenticated: false });
+import Loading from 'components/Loading';
 
-  const { isAuthenticated } = state;
+export default function AppContextProvider({ children }) {
+
+  const [handshakeConfirmed, setConfirmHandshake] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+
+  useEffect(() => {
+    const authToken = getCookie('authToken');
+    if (authToken) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    };
+    setConfirmHandshake(true);
+  }, []);
+
+  if (!handshakeConfirmed) {
+    return <Loading fullScreen/>;
+  }
+
   return (
     <AppContext.Provider
       value={{
