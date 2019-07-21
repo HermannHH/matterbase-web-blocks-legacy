45eb1890a5c7c83c4917f1f63dd3f8fca72d0fe3
Hermann <harris.hermann@gmail.com>
2019-07-21 12:41:45 +0200

    Update index.js

--- src/screens/public/Home/TheWhy/index.js
+++ src/screens/public/Home/TheWhy/index.js
@@ -5,21 +5,18 @@ function TheWhy({ theme }) {
   return (
     <div>
       <section
-        className="container rad-5 p-5 my-5"
-        style={{ backgroundColor: theme.colors.snowGrey, borderRadius: '5px' }}
+        className="container rad-5 p-5 my-5 selago-background"
+        style={{ backgroundColor: theme.colors.selago, borderRadius: '5px' }}
       >
         <div className="row d-flex justify-content-center align-items-center py-3">
           <div className="col-sm-12 d-flex justify-content-center flex-column text-center">
-              <h1>Why Matterbase?</h1>
+              <h1 className="dodger-blue-text font-weight-bold">Why Matterbase?</h1>
           </div>
         </div>
         <div className="row d-flex justify-content-center align-items-center">
-          <div className="col-sm-12 d-flex justify-content-center flex-column text-center">
+          <div className="col-sm-12 d-flex justify-content-center flex-column text-center px-5">
             <h4 className="tundora-text" style={{ lineHeight: '2.3rem' }}>
-              Calendars, planners & scheduling tools often operate in silos and can be hard to use. 
-            </h4>
-            <h4 className="tundora-text" style={{ lineHeight: '2.3rem' }}>
-              Matterbase transforms your day to day workflows to be more integrated & connected.
+              Calendars, planners & scheduling tools often operate in silos and can be hard to use. Matterbase transforms your day to day workflows to be more integrated & connected.
             </h4>
           </div>
         </div>
