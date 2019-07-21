fa71a43028ee589303eed6b1ffba446d6a7bf559
Hermann <harris.hermann@gmail.com>
2019-07-21 18:53:38 +0200

    Update index.js

--- src/screens/public/UseCases/UseCaseList/index.js
+++ src/screens/public/UseCases/UseCaseList/index.js
@@ -11,8 +11,8 @@ export default function UseCaseList() {
     <div>
       <div className="my-5 p-5 selago-background rad-5">
         <UseCase
-          heading="Use Cases"
-          text={<h4>We built Matterbase from the ground up with one thing in mind: Adaptability. <br/> Our users are very creative and have been using the platform in ingenious ways. One of these ways is to <strong className="dodger-blue-text">DYNAMIC VALUE:</strong></h4>}
+          heading="Meeting Assistant"
+          text={<div className="mb-5"><h4>Meetings often end up with outcomes lost in disorganised notes.</h4><h4>Attaching tasks & notes to the meeting event keeps all  thoughts & actions contextualised.</h4></div>}
           url="http://localhost:3000"
           buttonText="See more use cases"
           handleButtonClick={() => navigate(routes.public.useCases.path)}
@@ -20,8 +20,53 @@ export default function UseCaseList() {
       </div>
       <div className="my-5 p-5 snow-grey-background rad-5">
         <UseCase
-          heading="Use Cases"
-          text={<h4>We built Matterbase from the ground up with one thing in mind: Adaptability. <br/> Our users are very creative and have been using the platform in ingenious ways. One of these ways is to <strong className="dodger-blue-text">DYNAMIC VALUE:</strong></h4>}
+          heading="Project Planning"
+          text={<div className="mb-5"><h4>Project planning tools are often complicated. Project plans should be simple, to increase project success rates. Convert notes and ideas into actionable items which assist with  project completion.</h4></div>}
+          url="http://localhost:3000"
+          buttonText="See more use cases"
+          handleButtonClick={() => navigate(routes.public.useCases.path)}
+        />
+      </div>
+      <div className="my-5 p-5 selago-background rad-5">
+        <UseCase
+          heading="Productive Weeks"
+          text={<div className="mb-5"><h4>Planning your week in advance, helps increase  productivity. Create a Matter on Sunday night and jot down reminders and tasks to dedicate your time towards in the week to come. Then sit back and watch how this weekly process becomes a habit and removes stress and anxiety.</h4></div>}
+          url="http://localhost:3000"
+          buttonText="See more use cases"
+          handleButtonClick={() => navigate(routes.public.useCases.path)}
+        />
+      </div>
+      <div className="my-5 p-5 snow-grey-background rad-5">
+        <UseCase
+          heading="CRM"
+          text={<div className="mb-5"><h4>Create a timeline goal in which to close a deal. Never miss a deadline or a follow up and close deals in pre-planned cycles.</h4></div>}
+          url="http://localhost:3000"
+          buttonText="See more use cases"
+          handleButtonClick={() => navigate(routes.public.useCases.path)}
+        />
+      </div>
+      <div className="my-5 p-5 selago-background rad-5">
+        <UseCase
+          heading="Fund Raising Assistant"
+          text={<div className="mb-5"><h4>Raising investment for your business is tough. The last thing you need is for admin to add to the complexity. Never forget important tasks or notes on that perfect investor.</h4></div>}
+          url="http://localhost:3000"
+          buttonText="See more use cases"
+          handleButtonClick={() => navigate(routes.public.useCases.path)}
+        />
+      </div>
+      <div className="my-5 p-5 snow-grey-background rad-5">
+        <UseCase
+          heading="Goal Tracker"
+          text={<div className="mb-5"><h4>Whether it is money management, a side hustle or your fitness and health goals. Simplify and visualise your action items and progress using the adaptability of Matterbase.</h4></div>}
+          url="http://localhost:3000"
+          buttonText="See more use cases"
+          handleButtonClick={() => navigate(routes.public.useCases.path)}
+        />
+      </div>
+      <div className="my-5 p-5 selago-background rad-5">
+        <UseCase
+          heading="Financial Task Manager"
+          text={<div className="mb-5"><h4>Trying to get on top of your finances? Schedule a monthly Matter  to remind yourself of your goals. Set tasks to transfer into your savings account and pay a set amount into your credit card.</h4></div>}
           url="http://localhost:3000"
           buttonText="See more use cases"
           handleButtonClick={() => navigate(routes.public.useCases.path)}
