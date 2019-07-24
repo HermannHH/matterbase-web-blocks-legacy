a7c7f0f51d876886958e203ba808239f0f6e1795
Hermann <harris.hermann@gmail.com>
2019-07-24 09:04:03 +0200

    Update index.js

--- src/screens/public/UseCases/UseCaseList/index.js
+++ src/screens/public/UseCases/UseCaseList/index.js
@@ -12,7 +12,7 @@ import useCases from 'screens/public/useCases';
 export default function UseCaseList() {
   return (
     <div>
-      <div className="my-5 p-5 selago-background rad-5" id="meeting-assistant">
+      <div className="my-5 px-lg-3 py-5 selago-background rad-5" id="meeting-assistant">
         <UseCase
           heading="Meeting Assistant"
           text={<div className="mb-5"><h4>Meetings often end up with outcomes lost in disorganised notes.</h4><h4>Attaching tasks & notes to the meeting event keeps all  thoughts & actions contextualised.</h4></div>}
@@ -21,7 +21,7 @@ export default function UseCaseList() {
           handleButtonClick={() => navigate(routes.public.register.path)}
         />
       </div>
-      <div className="my-5 p-5 snow-grey-background rad-5" id="project-planning">
+      <div className="my-5 px-lg-3 py-5 snow-grey-background rad-5" id="project-planning">
         <UseCase
           heading="Project Planning"
           text={<div className="mb-5"><h4>Project planning tools are often complicated. Project plans should be simple, to increase project success rates. Convert notes and ideas into actionable items which assist with  project completion.</h4></div>}
@@ -30,7 +30,7 @@ export default function UseCaseList() {
           handleButtonClick={() => navigate(routes.public.register.path)}
         />
       </div>
-      <div className="my-5 p-5 selago-background rad-5" id="productive-weeks">
+      <div className="my-5 px-lg-3 py-5 selago-background rad-5" id="productive-weeks">
         <UseCase
           heading="Productive Weeks"
           text={<div className="mb-5"><h4>Planning your week in advance, helps increase  productivity. Create a Matter on Sunday night and jot down reminders and tasks to dedicate your time towards in the week to come. Then sit back and watch how this weekly process becomes a habit and removes stress and anxiety.</h4></div>}
@@ -39,7 +39,7 @@ export default function UseCaseList() {
           handleButtonClick={() => navigate(routes.public.register.path)}
         />
       </div>
-      <div className="my-5 p-5 snow-grey-background rad-5" id="crm">
+      <div className="my-5 px-lg-3 py-5 snow-grey-background rad-5" id="crm">
         <UseCase
           heading="CRM"
           text={<div className="mb-5"><h4>Create a timeline goal in which to close a deal. Never miss a deadline or a follow up and close deals in pre-planned cycles.</h4></div>}
@@ -48,7 +48,7 @@ export default function UseCaseList() {
           handleButtonClick={() => navigate(routes.public.register.path)}
         />
       </div>
-      <div className="my-5 p-5 selago-background rad-5" id="fund-raising-assistant">
+      <div className="my-5 px-lg-3 py-5 selago-background rad-5" id="fund-raising-assistant">
         <UseCase
           heading="Fund Raising Assistant"
           text={<div className="mb-5"><h4>Raising investment for your business is tough. The last thing you need is for admin to add to the complexity. Never forget important tasks or notes on that perfect investor.</h4></div>}
@@ -57,7 +57,7 @@ export default function UseCaseList() {
           handleButtonClick={() => navigate(routes.public.register.path)}
         />
       </div>
-      <div className="my-5 p-5 snow-grey-background rad-5" id="goal-tracker">
+      <div className="my-5 px-lg-3 py-5 snow-grey-background rad-5" id="goal-tracker">
         <UseCase
           heading="Goal Tracker"
           text={<div className="mb-5"><h4>Whether it is money management, a side hustle or your fitness and health goals. Simplify and visualise your action items and progress using the adaptability of Matterbase.</h4></div>}
@@ -66,7 +66,7 @@ export default function UseCaseList() {
           handleButtonClick={() => navigate(routes.public.register.path)}
         />
       </div>
-      <div className="my-5 p-5 selago-background rad-5" id="financial-task-manager">
+      <div className="my-5 px-lg-3 py-5 selago-background rad-5" id="financial-task-manager">
         <UseCase
           heading="Financial Task Manager"
           text={<div className="mb-5"><h4>Trying to get on top of your finances? Schedule a monthly Matter  to remind yourself of your goals. Set tasks to transfer into your savings account and pay a set amount into your credit card.</h4></div>}
