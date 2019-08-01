cf60ff59ba720bd7b60231dbb6a7f8c9e1842188
Hermann <harris.hermann@gmail.com>
2019-08-01 19:43:04 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {Helmet} from "react-helmet";
 
 import AppContext from 'contextuals/AppContext';
 import AppContextProvider from 'contextuals/AppContextProvider';
+import constants from 'utils/constants';
 // import PageNotFound from 'screens/errors/PageNotFound';
 import theme from 'utils/theme';
 import Public from './Public';
@@ -25,6 +26,20 @@ function App() {
                       <meta charSet="utf-8" />
                       <title>Matterbase</title>
                       <link rel="canonical" href="https://matterbase.io" />
+                      <meta name="title" content="Productivity building blocks for individuals & teams" />
+                      <meta name="description" content="Calendars, planners & scheduling tools often operate in silos and can be hard to use. Matterbase transforms your day to day workflows to be more integrated & connected." />
+
+                      <meta property="og:type" content="website" />
+                      <meta property="og:url" content="https://matterbase.io/" />
+                      <meta property="og:title" content="Productivity building blocks for individuals & teams" />
+                      <meta property="og:description" content="Calendars, planners & scheduling tools often operate in silos and can be hard to use. Matterbase transforms your day to day workflows to be more integrated & connected." />
+                      <meta property="og:image" content={`${constants.ENV.ASSET_BASE_PATH}/branding/og-image.png`} />
+
+                      <meta property="twitter:card" content="summary_large_image" />
+                      <meta property="twitter:url" content="https://matterbase.io/" />
+                      <meta property="twitter:title" content="Productivity building blocks for individuals & teams" />
+                      <meta property="twitter:description" content="Calendars, planners & scheduling tools often operate in silos and can be hard to use. Matterbase transforms your day to day workflows to be more integrated & connected." />
+                      <meta property="twitter:image" content={`${constants.ENV.ASSET_BASE_PATH}/branding/og-image.png`} />
                   </Helmet>
                   {!context.data.isAuthenticated && <Public appContext={context}/>}
                   {context.data.isAuthenticated && <Private appContext={context} />}
